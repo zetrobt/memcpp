a4f9913392fd42c3fe0a25e3c185c95db86adb0b
zetro <83398581+zetrobt@users.noreply.github.com>
2023-10-07 12:02:19 +0500

    Create memcpp.h

---
+++ include/memcpp.h
@@ -0,0 +1,27 @@
+#include <windows.h>
+#include <tlhelp32.h>
+#include <string>
+#include <assert.h>
+#include <vector>
+
+#ifndef TGAPI_MEMCPP_H
+#define TGAPI_MEMCPP_H
+
+namespace memcpp {
+
+    class memcpp {
+    public:
+        memcpp(std::string name);
+        uintptr_t get_module_from_name(std::string name);
+        template<typename T> WINBOOL read(uintptr_t address, T value);
+        template<typename T> WINBOOL write(uintptr_t address, T value);
+        std::vector<MODULEENTRY32> list_of_modules();
+        ~memcpp();
+    private:
+        int process_id;
+        HANDLE snapshot;
+    };
+
+}
+
+#endif
